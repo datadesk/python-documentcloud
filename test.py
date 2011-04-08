3a5de2f5b9b55da69e5b830f433d8d261a615bcc
palewire <ben.welsh@gmail.com>
2011-04-08 14:37:37 -0700

    Added a test to verify that all the expected document attributes exist in a get request

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ class BaseTest(unittest.TestCase):
 
 
 class DocumentSearchTest(BaseTest):
-
+    
     def test_search(self):
         """
         Test a search.
@@ -20,22 +20,45 @@ class DocumentSearchTest(BaseTest):
         obj_list = documentcloud.documents.search(self.test_search)
         self.assertEqual(type(obj_list), type([]))
     
-    def test_multipage_search(self):
-        """
-        Test a search that will return more than a single page of results.
-        """
-        obj_list = documentcloud.documents.search("johnson")
-        self.assertTrue(len(obj_list) > 1000)
+#    def test_multipage_search(self):
+#        """
+#        Test a search that will return more than a single page of results.
+#        """
+#        obj_list = documentcloud.documents.search("johnson")
+#        self.assertTrue(len(obj_list) > 1000)
 
 
 class DocumentGetTest(BaseTest):
-
+    
     def test_get(self):
         """
         Test a get request for a particular document.
         """
         obj = documentcloud.documents.get(self.test_id)
         self.assertEqual(type(obj), Document)
+    
+    def test_attrs(self):
+        """
+        Verify that all the Document attributes exist.
+        """
+        obj = documentcloud.documents.get(self.test_id)
+        attr_list = [
+            'access',
+            'annotations',
+            'canonical_url',
+            'contributor',
+            'contributor_organization',
+            'created_at',
+            'description',
+            'id',
+            'pages',
+            'resources',
+            'sections',
+            'source',
+            'title',
+            'updated_at',
+        ]
+        [self.assertTrue(hasattr(obj, attr)) for attr in attr_list]
 
 
 if __name__ == '__main__':
