bdacde2d8f1d8e0657bb2545d30e0cf694925947
palewire <ben.welsh@gmail.com>
2011-04-13 07:09:35 -0700

    Started drafting an latimes sphinx theme

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Table Stacker'
+project = u'python-documentcloud'
 copyright = u'2011, Los Angeles Times'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'latimes'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
