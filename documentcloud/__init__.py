cd84fb138c4c8e39ee9de77d42b8528d356edf5a
Ben Welsh <ben.welsh@gmail.com>
2013-08-22 07:47:42 -0700

    Docstring update

--- documentcloud/__init__.py
+++ documentcloud/__init__.py
@@ -7,6 +7,7 @@ authorization is required to interact with private documents.
 
 Further documentation:
 
+    http://datadesk.github.io/python-documentcloud/
     https://www.documentcloud.org/help/api
 
 """
