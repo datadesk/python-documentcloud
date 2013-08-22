dc8113dbc61255189a33dbaed9e1988c482b7263
Ben Welsh <ben.welsh@gmail.com>
2013-08-22 07:47:09 -0700

    Typo

--- documentcloud/__init__.py
+++ documentcloud/__init__.py
@@ -842,7 +842,7 @@ class Document(BaseAPIObject):
 
 class DocumentSet(list):
     """
-    A custom class for project lists associated with projects.
+    A custom class for document lists associated with projects.
     
     Allows some tweaks, like preventing duplicate documents
     from getting into the list and ensuring that only Document
