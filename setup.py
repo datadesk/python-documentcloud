14c56532871feb4b2ad9347d0980e84a3f22b80f
palewire <ben.welsh@gmail.com>
2018-12-03 13:34:33 -0800

    Updated version numbers in the tests

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
     ),
 )
